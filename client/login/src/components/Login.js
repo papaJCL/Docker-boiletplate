f69beb7d143e043710d2d5e1aeee11268c9cbd7f
papaJCL <jeremylessermore@gmail.com>
2020-09-19 03:20:44 -0600

    Big UI changes

--- client/login/src/components/Login.js
+++ client/login/src/components/Login.js
@@ -2,28 +2,90 @@ import React, { useEffect, useState , Fragment} from "react";
 import { Link, Redirect } from "react-router-dom";
 import styled from 'styled-components'
 
-export const Header = styled.h1`
+const Header = styled.h1`
   font-size:2.5rem;
-  text-transform: uppercase;
   text-align: center;
-  font-weight:700;
+  font-weight:300;
   margin-top:1.5em;
+  padding-bottom:1.5rem;
   font-family: 'Raleway', sans-serif;
-  letter-spacing: 2px;
+  z-index: 2;
+  color: #2F4F4F;
+  position: relative;
 `
 
-export const AboutStyles = styled.div`
+const Background = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
   align-items: center;
-    background: red no-repeat center center ;
-    //background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    display: flex;
-    flex-direction: column;
-    //flex-wrap: wrap;
-    overflow-x: hidden;
+
 `
+
+const Box = styled.div`
+  background-color: #fafafa;
+  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+  text-align: center;
+  padding: 40px;
+  
+
+  @media (max-width: 576px) {
+    width: 100%
+    border: none;
+    background-color: #fafafa;
+    box-shadow: none;
+  }
+}
+`
+
+const Input = styled.input`
+  width: 80%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: black;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid grey;
+  outline: none;
+  background: transparent;
+
+  @media (max-width: 576px) {
+    width: 100%
+  }
+`
+
+const Button = styled.button`
+  width: 80%;
+  padding: 10px 0;
+  font-size: 16px;
+  background-color: #00580e;
+  color: #fafafa;
+  margin-bottom: 30px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: transform .1s linear;
+
+  @media (max-width: 576px) {
+    width: 100%
+  }
+  &:hover {
+        transform: scale(1.05);
+    },
+`
+
+const StyledLink = styled(Link)`
+  font-size: 1.3rem;
+  color: #696969;
+  transition: color 0.1s linear;
+  font-family: 'Raleway', sans-serif;
+
+
+  &:hover {
+    text-decoration: none;
+    color: grey;
+  }
+`;
 
 
 const Login = ({setAuth}) =>{
@@ -68,27 +130,38 @@ const Login = ({setAuth}) =>{
 	}
 
 	return(
-    <AboutStyles>
-			<Header> Login</Header>
-			<form onSubmit={onSubmitForm}>
-        		<input
-        			placeholder="email" 
-          			type="text"
-          			name="email"
-          			value={email}
-          			onChange={e => onChange(e)}
-        		/>
-        		<input
-        			placeholder="password" 
-          			type="password"
-          			name="password"
-          			value={password}
-          			onChange={e => onChange(e)}
-        		/>
-        		<button>Submit</button>
-      		</form>
-			<Link to="/register">register</Link>
-      </AboutStyles>
+    <Background>
+      <div className="container">
+        <Box>
+          <Header>
+            Login
+          </Header>
+          <form onSubmit={onSubmitForm}>
+          <Input
+              placeholder="email" 
+                type="text"
+                name="email"
+                value={email}
+                onChange={e => onChange(e)}
+            />
+            <br />
+            <Input
+              placeholder="password" 
+                type="password"
+                name="password"
+                value={password}
+                onChange={e => onChange(e)}
+            />
+            <br />
+            <Button>Submit</Button>
+          </form>
+          <br />
+			   <StyledLink to="/register">Register</StyledLink>
+        </Box>
+      </div>
+    </Background>
+      
+
 	);
 }
 
--- client/login/src/components/Register.js
+++ client/login/src/components/Register.js
@@ -1,6 +1,92 @@
 import React, { useEffect, useState , Fragment } from "react";
 import { toast } from "react-toastify";
 import { Link, Redirect } from "react-router-dom";
+import styled from 'styled-components'
+
+const Header = styled.h1`
+  font-size:2.5rem;
+  text-align: center;
+  font-weight:300;
+  margin-top:1.5em;
+  padding-bottom:1.5rem;
+  font-family: 'Raleway', sans-serif;
+  z-index: 2;
+  color: #2F4F4F;
+  position: relative;
+`
+
+const Background = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+`
+
+const Box = styled.div`
+  background-color: #fafafa;
+  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+  text-align: center;
+  padding: 40px;
+  
+
+  @media (max-width: 576px) {
+    width: 100%
+    border: none;
+    background-color: #fafafa;
+    box-shadow: none;
+  }
+}
+`
+
+const Input = styled.input`
+  width: 80%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: black;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid grey;
+  outline: none;
+  background: transparent;
+
+  @media (max-width: 576px) {
+    width: 100%
+  }
+`
+
+const Button = styled.button`
+  width: 80%;
+  padding: 10px 0;
+  font-size: 16px;
+  background-color: #00580e;
+  color: #fafafa;
+  margin-bottom: 30px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: transform .1s linear;
+
+  @media (max-width: 576px) {
+    width: 100%
+  }
+  &:hover {
+        transform: scale(1.05);
+    },
+`
+
+const StyledLink = styled(Link)`
+  font-size: 1.3rem;
+  color: #696969;
+  transition: color 0.1s linear;
+  font-family: 'Raleway', sans-serif;
+
+
+  &:hover {
+    text-decoration: none;
+    color: grey;
+  }
+`;
 
 const Register = ({setAuth}) =>{
 
@@ -41,22 +127,28 @@ const Register = ({setAuth}) =>{
 	}
 
 	return(
-		<Fragment> 
-			<h1 className="text-center my-5">Register</h1>
-			<form onSubmit={onSubmitForm}>
-				<input type="email" name="email" placeholder="email" 
+		<Background>
+      		<div className="container">
+      			<Box>
+					<Header>
+						Register
+					</Header>
+					<form onSubmit={onSubmitForm}>
+					<Input type="email" name="email" placeholder="email" 
 					className="form-control my-3" value={email} onChange={e => onChange(e)}
-				/>
-				<input type="password" name="password" placeholder="password" 
+					/>
+					<Input type="password" name="password" placeholder="password" 
 					className="form-control my-3" value={password} onChange={e => onChange(e)}
-				/>
-				<input type="text" name="name" placeholder="name" 
+					/>
+					<Input type="text" name="name" placeholder="name" 
 					className="form-control my-3" value={name} onChange={e => onChange(e)}
-				/>
-				<button className="btn-success btn-block">Submit</button>
+					/>
+					<Button className="btn-success btn-block">Submit</Button>
 			</form>
-			<Link to="/login">login</Link>
-		</Fragment>
+			<StyledLink to="/login">Login</StyledLink>
+			</Box>
+			</div>
+    </Background>
 	);
 }
 
